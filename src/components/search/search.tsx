3bedfd109abe768ab31c1efc3d0e6f2356633aec
Brian <brian@briankiernan.com>
2022-08-17 10:52:25 -0400

    refactor(search): update spacing

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -4,6 +4,7 @@ import Loading from 'components/loading';
 import MediaFilter from 'components/media-filter';
 import Breadcrumbs from 'components/breadcrumbs';
 import MediaItem from 'components/media-item';
+import Pagination from 'components/pagination';
 import './search.css';
 
 export const Search: React.FC = () => {
@@ -15,25 +16,30 @@ export const Search: React.FC = () => {
   return (
     <section id='search-container'>
       <MediaFilter />
-      <div>
-        <Breadcrumbs {...{ search }} />
-        <div id='search-results'>
-          {!!results.length ? (
-            <div id='search-meta'>
-              <h2>Showing results for "{params.q}":</h2>
-              <p>
-                Displaying page {params.page} of {pages}
-              </p>
-            </div>
-          ) : (
-            <h2>No results found.</h2>
-          )}
-        </div>
-        <main id='search-results-container'>
+      <div id='search-wrapper'>
+        <header>
+          <Breadcrumbs {...{ search }} />
+          <div>
+            {!!results.length ? (
+              <div id='search-meta'>
+                <h2>Showing results for "{params.q}":</h2>
+                <p>
+                  Displaying page {params.page} of {pages}
+                </p>
+              </div>
+            ) : (
+              <h2>No results found.</h2>
+            )}
+          </div>
+        </header>
+        <main>
           {results.map((item, i) => (
             <MediaItem key={item.data[0].nasa_id + i} {...item} />
           ))}
         </main>
+        <footer>
+          <Pagination />
+        </footer>
       </div>
     </section>
   );
