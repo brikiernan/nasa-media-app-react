f6b3234553feb025a8b07313948632d1c9c1496a
Brian <brian@briankiernan.com>
2022-08-18 23:35:42 -0400

    feature(pagination): update feature

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -3,70 +3,99 @@ import { Link } from 'react-router-dom';
 
 import { useAppContext } from 'providers';
 import { setSearchUrl } from 'lib/utils';
+import PaginationItem from './pagination-item';
 import './pagination.css';
 
 export const Pagination: React.FC = () => {
-  const [paginated, setPaginated] = useState<string[]>([]);
-  const [paths, setPaths] = useState<string[]>([]);
-  const { pages, params } = useAppContext();
-  const currentPage = +params.page;
-  const isPageOne = currentPage === 1;
-  const isLastPage = currentPage === pages;
+  const [paginated, setPaginated] = useState<any[]>([]);
+  const { pages: count, params } = useAppContext();
+  const page = +params.page;
+  const isPageOne = page === 1;
+  const isLastPage = page === count;
 
   useEffect(() => {
-    /**
-     * This string array should be passed in as a prop
-     * to make the component reusable.
-     **/
     const hrefs: string[] = [];
-    for (let i = 0; pages > i; i++) {
+    for (let i = 0; count > i; i++) {
       const page = (i + 1).toString();
       const searchUrl = setSearchUrl({ ...params, page });
       hrefs.push(searchUrl);
     }
-    /* end */
 
-    setPaths(hrefs);
-    const truncatedHrefs = [...hrefs.slice(0, 4), '...', ...hrefs.slice(-2)];
-    setPaginated(truncatedHrefs);
-  }, [pages, params]);
+    setPaginated(hrefs);
+  }, [count, params]);
 
-  if (!!!pages) return null;
+  if (!count) return null;
 
   return (
-    <ul id='pagination'>
-      {!isPageOne && (
-        <li>
-          <Link to={setSearchUrl({ ...params, page: String(currentPage - 1) })}>
-            {'< '}Prev
-          </Link>
-        </li>
-      )}
-      {paginated.map(href => {
-        const page = paths.indexOf(href) + 1;
-        const isCurrentPage = page === +params.page;
+    <nav id='pagination'>
+      <Link
+        className={isPageOne ? 'link-disabled' : ''}
+        to={setSearchUrl({ ...params, page: String(page - 1) })}
+      >
+        {'< '}Prev
+      </Link>
+      <ul>
+        {paginated.map((href, i) => {
+          const max = 5;
+          const pageNumber = i + 1;
+          const isCurrentPage = page === pageNumber;
+          const isFirstPage = i === 0;
+          const isLastPage = i === count - 1;
+          const isRightBeforeLastPage = i === count - 2;
+          const isRightAfterCurrentPage = page + 1 === pageNumber;
+          const isRightBeforeCurrentPage = page - 1 === pageNumber;
 
-        return (
-          <li key={href}>
-            {page ? (
-              isCurrentPage ? (
-                page
-              ) : (
-                <Link to={href}>{page}</Link>
-              )
-            ) : (
-              <span>{href}</span>
-            )}
-          </li>
-        );
-      })}
-      {!isLastPage && (
-        <li>
-          <Link to={setSearchUrl({ ...params, page: String(currentPage + 1) })}>
-            Next{' >'}
-          </Link>
-        </li>
-      )}
-    </ul>
+          if (isCurrentPage) {
+            return <li key={pageNumber}>{pageNumber}</li>;
+          }
+
+          if (count <= 7) {
+            return <PaginationItem key={href} href={href} page={pageNumber} />;
+          }
+
+          if (page >= max && pageNumber === 2) {
+            return <li key={pageNumber}>...</li>;
+          }
+
+          if (
+            isRightBeforeCurrentPage ||
+            isRightAfterCurrentPage ||
+            isFirstPage ||
+            isLastPage
+          ) {
+            return <PaginationItem key={href} href={href} page={pageNumber} />;
+          }
+
+          if (
+            (page >= max && pageNumber === 3) ||
+            (page >= max && pageNumber === 4) ||
+            (page >= max && pageNumber === 5)
+          ) {
+            return null;
+          }
+
+          if (
+            pageNumber <= max ||
+            (isRightBeforeLastPage && page === count - 3) ||
+            ((pageNumber === count - 4 ||
+              pageNumber === count - 3 ||
+              pageNumber === count - 2) &&
+              page > count - 4)
+          ) {
+            return <PaginationItem key={href} href={href} page={pageNumber} />;
+          }
+
+          if (isRightBeforeLastPage) return <li key={pageNumber}>...</li>;
+
+          return null;
+        })}
+      </ul>
+      <Link
+        className={isLastPage ? 'link-disabled' : ''}
+        to={setSearchUrl({ ...params, page: String(page + 1) })}
+      >
+        Next{' >'}
+      </Link>
+    </nav>
   );
 };
